934626ac88089b1958ccc76c5f8d8a1641b5b886
AnanyaKarunakar <karunakark1729@gmail.com>
2025-10-31 20:52:03 +0530

    Hello-world for CICD

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+from app import get_message
+
+class TestApp(unittest.TestCase):
+    def test_get_message(self):
+        self.assertEqual(get_message(), "Hello, World!")
+
+if __name__ == "__main__":
+    unittest.main()
